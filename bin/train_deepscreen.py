0db20992b33c4923d00813132ddca03accccf659
Kerem Örer <161973110+thecubedaryl@users.noreply.github.com>
2024-08-01 12:51:46 +0300

    Update2 train_deepscreen.py

--- bin/train_deepscreen.py
+++ bin/train_deepscreen.py
@@ -13,13 +13,20 @@ from models import CNNModel1
 from torch.autograd import Variable
 from data_processing import get_train_test_val_data_loaders
 from evaluation_metrics import prec_rec_f1_acc_mcc, get_list_of_scores
+from rdkit import Chem
+from sklearn.metrics import roc_auc_score, average_precision_score
+from rdkit.Chem import Draw
+from concurrent.futures import ProcessPoolExecutor
+from data_processing import generate_images
+import time
+import argparse
+import multiprocessing
+import wandb
 
 warnings.filterwarnings(action='ignore')
 torch.manual_seed(123)
 np.random.seed(123)
 use_gpu = torch.cuda.is_available()
-
-from torch.utils.tensorboard import SummaryWriter
 
 
 current_path_beginning = os.getcwd().split("DEEPScreen")[0]
@@ -30,8 +37,10 @@ training_files_path = "{}/training_files".format(project_file_path)
 result_files_path = "{}/result_files".format(project_file_path)
 trained_models_path = "{}/trained_models".format(project_file_path)
 
+
+
 def save_best_model_predictions(experiment_name, epoch, validation_scores_dict, test_scores_dict, model, project_file_path, target_id, str_arguments,
-                                                                                   all_test_comp_ids, test_labels, test_predictions):
+                                all_test_comp_ids, test_labels, test_predictions):
 
     if not os.path.exists(os.path.join(trained_models_path, experiment_name)):
         os.makedirs(os.path.join(trained_models_path, experiment_name))
@@ -39,7 +48,6 @@ def save_best_model_predictions(experiment_name, epoch, validation_scores_dict, 
     torch.save(model.state_dict(),
                "{}/{}/{}_best_val-{}-state_dict.pth".format(trained_models_path, experiment_name,
                                                                                target_id, str_arguments))
-    # print(all_test_comp_ids)
     str_test_predictions = "CompoundID\tLabel\tPred\n"
     for ind in range(len(all_test_comp_ids)):
         str_test_predictions += "{}\t{}\t{}\n".format(all_test_comp_ids[ind],
@@ -48,8 +56,6 @@ def save_best_model_predictions(experiment_name, epoch, validation_scores_dict, 
     best_test_performance_dict = test_scores_dict
     best_test_predictions = str_test_predictions
     return validation_scores_dict, best_test_performance_dict, best_test_predictions, str_test_predictions
-
-
 
 def get_device(cuda_selection):
     device = "cpu"
@@ -60,13 +66,13 @@ def get_device(cuda_selection):
         print("CPU is available on this device!")
     return device
 
-
 def calculate_val_test_loss(model, criterion, data_loader, device):
     total_count = 0
     total_loss = 0.0
     all_comp_ids = []
     all_labels = []
     all_predictions = []
+    all_pred_probs = []
     for i, data in enumerate(data_loader):
         img_arrs, labels, comp_ids = data
         img_arrs, labels = torch.tensor(img_arrs).type(torch.FloatTensor).to(device), torch.tensor(labels).to(device)
@@ -78,15 +84,13 @@ def calculate_val_test_loss(model, criterion, data_loader, device):
         _, preds = torch.max(y_pred, 1)
         all_labels.extend(list(labels))
         all_predictions.extend(list(preds))
+        all_pred_probs.extend(y_pred.detach().cpu().numpy())
 
 
-    return total_loss, total_count, all_comp_ids, all_labels, all_predictions
+    return total_loss, total_count, all_comp_ids, all_labels, all_predictions,all_pred_probs
 
+def train_validation_test_training(target_id, model_name, fully_layer_1, fully_layer_2, learning_rate, batch_size, drop_rate, n_epoch, experiment_name, cuda_selection, dataset_file, target_prediction_dataset_path, max_cores):
 
-def train_validation_test_training(target_id, model_name, fully_layer_1, fully_layer_2, learning_rate, batch_size, drop_rate, n_epoch, experiment_name, cuda_selection):
-    
-    
-    # Systems have different floating point precision for scientifing notation (e notation)
     arguments = ["{:.16f}".format(argm).rstrip('0') if type(argm)==float else str(argm) for argm in
                  [target_id, model_name, fully_layer_1, fully_layer_2, learning_rate, batch_size, drop_rate, n_epoch, experiment_name]]
 
@@ -96,16 +100,25 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
     str_arguments = "-".join(arguments)
     print("Arguments:", str_arguments)
     
-    # To log loss (graphical)
-    writer = SummaryWriter(comment = str_arguments+"_"+str(cuda_selection))
 
-    
+    wandb.init(project='my_project', name=experiment_name, config={
+        "target_id": target_id,
+        "model_name": model_name,
+        "fully_layer_1": fully_layer_1,
+        "fully_layer_2": fully_layer_2,
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "drop_rate": drop_rate,
+        "n_epoch": n_epoch,
+        "cuda_selection": "CPU"
+    })
+
+
     device = get_device(cuda_selection)
     exp_path = os.path.join(result_files_path, "experiments", experiment_name)
 
     if not os.path.exists(exp_path):
         os.makedirs(exp_path)
-
 
     best_val_test_result_fl = open(
         "{}/best_val_test_performance_results-{}.txt".format(exp_path,str_arguments), "w")
@@ -132,11 +145,10 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
         batch_number = 0
         all_training_labels = []
         all_training_preds = []
+        all_training_probs = []
         print("Training mode:", model.training)
         for i, data in enumerate(train_loader):
             batch_number += 1
-            # print(batch_number)
-            # clear gradient DO NOT forget you fool!
             optimizer.zero_grad()
             img_arrs, labels, comp_ids = data
             img_arrs, labels = torch.tensor(img_arrs).type(torch.FloatTensor).to(device), torch.tensor(labels).to(device)
@@ -146,6 +158,8 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
             _, preds = torch.max(y_pred, 1)
             all_training_labels.extend(list(labels))
             all_training_preds.extend(list(preds))
+            all_training_probs.extend(y_pred.detach().cpu().numpy())
+
 
             loss = criterion(y_pred.squeeze(), labels)
             total_training_loss += float(loss.item())
@@ -153,20 +167,28 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
             optimizer.step()
         print("Epoch {} training loss:".format(epoch), total_training_loss)
         
-        # To log loss (graphical)
-        writer.add_scalar("Loss/train", total_training_loss, epoch)
+        wandb.log({"Loss/train": total_training_loss, "epoch": epoch})
         
         training_perf_dict = dict()
         try:
-        	training_perf_dict = prec_rec_f1_acc_mcc(all_training_labels, all_training_preds)
+            training_perf_dict = prec_rec_f1_acc_mcc(all_training_labels, all_training_preds)
         except:
-        	print("There was a problem during training performance calculation!")
-        # print(training_perf_dict)
+            print("There was a problem during training performance calculation!")
+        
+        training_roc_auc = roc_auc_score(all_training_labels, np.array(all_training_probs)[:, 1])
+        training_pr_auc = average_precision_score(all_training_labels, np.array(all_training_probs)[:, 1])
+        training_perf_dict["ROC AUC"] = training_roc_auc
+        training_perf_dict["PR AUC"] = training_pr_auc
+
+        for metric, value in training_perf_dict.items():
+            wandb.log({f"Train/{metric}": value, "epoch": epoch})
+
+
         model.eval()
-        with torch.no_grad():  # torch.set_grad_enabled(False):
+        with torch.no_grad():
             print("Validation mode:", not model.training)
 
-            total_val_loss, total_val_count, all_val_comp_ids, all_val_labels, val_predictions = calculate_val_test_loss(model, criterion, valid_loader, device)
+            total_val_loss, total_val_count, all_val_comp_ids, all_val_labels, val_predictions,val_pred_probs = calculate_val_test_loss(model, criterion, valid_loader, device)
 
             val_perf_dict = dict()
             val_perf_dict["MCC"] = 0.0
@@ -176,7 +198,15 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
                 print("There was a problem during validation performance calculation!")
 
 
-            total_test_loss, total_test_count, all_test_comp_ids, all_test_labels, test_predictions = calculate_val_test_loss(
+            val_roc_auc = roc_auc_score(all_val_labels, np.array(val_pred_probs)[:, 1])
+            val_pr_auc = average_precision_score(all_val_labels, np.array(val_pred_probs)[:, 1])
+            val_perf_dict["ROC AUC"] = val_roc_auc
+            val_perf_dict["PR AUC"] = val_pr_auc
+
+            for metric, value in val_perf_dict.items():
+                wandb.log({f"Validation/{metric}": value, "epoch": epoch})
+
+            total_test_loss, total_test_count, all_test_comp_ids, all_test_labels, test_predictions,test_pred_probs = calculate_val_test_loss(
                 model, criterion, test_loader, device)
 
             test_perf_dict = dict()
@@ -185,6 +215,15 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
                 test_perf_dict = prec_rec_f1_acc_mcc(all_test_labels, test_predictions)
             except:
                 print("There was a problem during test performance calculation!")
+
+
+            test_roc_auc = roc_auc_score(all_test_labels, np.array(test_pred_probs)[:, 1])
+            test_pr_auc = average_precision_score(all_test_labels, np.array(test_pred_probs)[:, 1])
+            test_perf_dict["ROC AUC"] = test_roc_auc
+            test_perf_dict["PR AUC"] = test_pr_auc
+
+            for metric, value in test_perf_dict.items():
+                wandb.log({f"Test/{metric}": value, "epoch": epoch})
 
             if val_perf_dict["MCC"] > best_val_mcc_score:
                 best_val_mcc_score = val_perf_dict["MCC"]
@@ -195,11 +234,8 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
                     model,project_file_path, target_id, str_arguments,
                     all_test_comp_ids, all_test_labels, test_predictions)
             
-            # To log loss (graphical)
-            writer.add_scalar("Loss/validation", total_val_loss, epoch)
-            
-            # To log loss (graphical)
-            writer.add_scalar("Loss/test", total_test_loss, epoch)
+        wandb.log({"Loss/validation": total_val_loss, "epoch": epoch})
+        wandb.log({"Loss/test": total_test_loss, "epoch": epoch})
 
         if epoch == n_epoch - 1:
             score_list = get_list_of_scores()
@@ -209,11 +245,10 @@ def train_validation_test_training(target_id, model_name, fully_layer_1, fully_l
 
             best_val_test_result_fl.close()
             best_val_test_prediction_fl.close()
-
-
         
-        writer.flush()
+        
     
-    
-    writer.close()
+    wandb.finish()
+
+    generate_images(dataset_file, target_id, target_prediction_dataset_path, max_cores)
 
